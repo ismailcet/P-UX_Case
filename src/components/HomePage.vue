d27401944ca45ce4ac5cfb726daaa7f2b5d5ed83
ismailcet <ismailcet3@gmail.com>
2023-03-02 02:04:24 +0300

    P-UX Vue.js Task Case Header finished.

---
+++ src/components/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+    <main class="flex flex-row py-[3.25rem] px-[0.75rem] gap-[0.625rem]">
+        <Navigation />
+        <Content />
+    </main>
+</template>
+<script>
+import Navigation from "@/components/Navigation.vue"
+import Content from "@/components/Content.vue"
+export default {
+    components:{
+        Navigation,Content
+    }
+}
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="w-[16.3125rem] h-[63.75rem] rounded-3xl navbar px-[1.25rem] py-[1.375rem]">
+        <div class="logo_section">
+            <img src="@/assets/logo.png" alt="Logo" class="logo mb-5">
+        </div>
+        <hr>
+        <div class="navbar_item_section pt-5">
+            <ul class="navbar_items">
+                <li class="navbar_item text-white font-bold text-base p-2 flex items-center gap-3 mb-6"><fa icon="fa-solid fa-list-check" /> Görevlerim</li>
+                <li class="navbar_item text-white font-bold text-base p-2 flex items-center gap-3 mb-6"><fa icon="fa-solid fa-folder-open" /> Projelerim</li>
+                <li class="navbar_item text-white font-bold text-base p-2 flex items-center gap-3 mb-6"><fa icon="fa-solid fa-briefcase" /> Firmalar</li>
+                <li class="navbar_item text-white font-bold text-base p-2 flex items-center gap-3 mb-6"><fa icon="fa-solid fa-calendar" /> Takvim</li>
+                <li class="navbar_item text-white font-bold text-base p-2 flex items-center gap-3 mb-6"><fa icon="fa-solid fa-user-group" /> Kişiler</li>
+                <li class="navbar_item text-white font-bold text-base p-2 flex items-center gap-3 mb-6"><fa icon="fa-solid fa-chart-line" /> Raporlar</li>
+                <li class="navbar_item text-white font-bold text-base p-2 flex items-center gap-3"><fa icon="fa-solid fa-timeline" /> Otomasyon</li>
+            </ul>
+        </div>
+    </header>
+</template>
+<script>
+
+export default {
+    
+}
+</script>
